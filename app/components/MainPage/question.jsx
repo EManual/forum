032411d88da0fe638352b6c03393d8bbfda854fe
Jayin Ton <tonjayin@gmail.com>
2016-04-08 10:41:31 +0800

    add form

--- app/components/MainPage/question.jsx
+++ app/components/MainPage/question.jsx
@@ -11,7 +11,32 @@ export default class MainPage extends React.Component {
       <main>
         <div className="weui_tab">
                 <div className="weui_tab_bd">
+                      <header style={{textAlign: 'center', marginTop: '10%', color: '#3cc51f'}}>
+                          <h2 className="page_title">发布问题</h2>
+                      </header>
+                      <div className="weui_cells_title">标题</div>
+                      <div className="weui_cells weui_cells_form">
+                          <div className="weui_cell">
+                              <div className="weui_cell_bd weui_cell_primary">
+                                  <input className="weui_input" type="text" placeholder="标题"/>
+                              </div>
+                          </div>
+                        
+                      </div>
                       
+                      <div className="weui_cells_title">问题描述</div>
+                      <div className="weui_cells weui_cells_form">
+                            <div className="weui_cell">
+                                <div className="weui_cell_bd weui_cell_primary">
+                                    <textarea className="weui_textarea" placeholder="请输入你的问题描述" rows="4"></textarea>
+                                    <div className="weui_textarea_counter"><span>0</span>/200</div>
+                                </div>
+                            </div>
+                       </div>
+                       <div style={{borderWidth: '0',margin: '8% 4% 4%'}}>
+                          <button href="javascript:;" className="weui_btn weui_btn_plain_primary">发布</button>
+                       </div>
+                       
                 </div>
                 <div className="weui_tabbar">
                     <a href="#/lastest" className="weui_tabbar_item">
