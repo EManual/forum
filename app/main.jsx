1b106557f4bc3d2ac56206d5daa7f8f2945052fa
Jayin Ton <tonjayin@gmail.com>
2016-05-02 17:12:26 +0800

    Update

--- app/main.jsx
+++ app/main.jsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
-import LastestPage from './components/MainPage/lastest';
-import FollowPage from './components/MainPage/follow';
-import MePage from './components/MainPage/me';
-import QuestionPage from './components/MainPage/question';
+import LastestPage from './components/MainPage/lastest'
+import FollowPage from './components/MainPage/follow'
+import MePage from './components/MainPage/me'
+import QuestionPage from './components/MainPage/question'
 
-import LoginPage from './components/LoginPage';
-import RegisterPage from './components/RegisterPage';
-import NoMatchPage from './components/NoMatchPage';
-import QuestionDetailPage from './components/QuestionDetailPage';
+import LoginPage from './components/LoginPage'
+import RegisterPage from './components/RegisterPage'
+import NoMatchPage from './components/NoMatchPage'
+import QuestionDetailPage from './components/QuestionDetailPage'
 import FeedbackPage from './components/FeedbackPage'
 
-require('./css/weui-0.4.0.css');
+require('./css/weui-0.4.0.css')
+require('./css/normalize-4.0.0.css')
+require('./css/main.css')
 
 ReactDOM.render((
   <Router history={hashHistory}>
